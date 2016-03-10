d129c91a57026c0e28ec4b1c8bb5ffeeb63d1f92
Yana <Yana>
2016-03-10 09:42:38 +0200

    cardview with recyclerview

--- app/src/main/java/com/qoobico/remindme/fragment/HistoryFragment.java
+++ app/src/main/java/com/qoobico/remindme/fragment/HistoryFragment.java
@@ -3,15 +3,21 @@ package com.qoobico.remindme.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.qoobico.remindme.R;
+import com.qoobico.remindme.adapter.RemindListAdapter;
+import com.qoobico.remindme.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends AbstractTabFragment {
-    private static final int LAYOUT = R.layout.fragmentexample;
+    private static final int LAYOUT = R.layout.fragment_history;
 
     public static HistoryFragment getInstance(Context context) {
         Bundle args = new Bundle();
@@ -29,7 +35,20 @@ public class HistoryFragment extends AbstractTabFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
 
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(new RemindListAdapter(createMockData()));
+
         return view;
+    }
+
+    private List<RemindDTO> createMockData() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("first"));
+        data.add(new RemindDTO("last"));
+        data.add(new RemindDTO("blabla"));
+
+        return data;
     }
 
     public void setContext(Context context) {
